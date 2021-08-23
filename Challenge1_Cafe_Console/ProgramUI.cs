eaca5fe4dbe1be6cc9eface63819cbde56fa44f4
Duncan Summers <djsummers36@gmail.com>
2021-08-23 13:05:10 -0400

    removed dictionary from menu and repository

--- Challenge1_Cafe_Console/ProgramUI.cs
+++ Challenge1_Cafe_Console/ProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoldBadgeChallenges;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Challenge1_Cafe_Console
 {
     public class ProgramUI
     {
+        private readonly MenuRepository _menu = new MenuRepository();
         public void Run()
         {
             Menu();
@@ -17,6 +19,21 @@ namespace Challenge1_Cafe_Console
             bool viewingMenu = true;
             while (viewingMenu)
             {
+                Console.WriteLine("Welcome to Komodo Cafe!\n" +
+                    "Menu:\n" +
+                    "1. View Menu\n" +
+                    "2. Add new menu item\n" +
+                    "3. Remove menu item\n" +
+                    "4. Exit");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        ReadMenu()
+                        break;
+                    case "2"
+                        
 
             }
         }
