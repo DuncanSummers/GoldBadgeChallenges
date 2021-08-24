dfdde825246d8dfbcf0b035865f81c73ba7c8d5a
Duncan Summers <djsummers36@gmail.com>
2021-08-24 10:31:58 -0400

    ClaimsRepository made

--- Challenge2_Claims_Tests/RepositoryTests.cs
+++ Challenge2_Claims_Tests/RepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Challenge2_Claims_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Challenge2_Claims_Tests
@@ -6,6 +7,8 @@ namespace Challenge2_Claims_Tests
     [TestClass]
     public class RepositoryTests
     {
+        private ClaimsRepository _claims
+        
         [TestMethod]
         public void TestMethod1()
         {
