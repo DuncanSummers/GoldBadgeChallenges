9d0f872aa6c8bff8f97118ec6480459e78ad4e87
Duncan Summers <djsummers36@gmail.com>
2021-08-24 09:17:14 -0400

    Claims challenge projects added

---
+++ Challenge2_Claims_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge2_Claims_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Challenge2_Claims_Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a12987ed-10da-4331-a123-a3b7bfec8cfb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Challenge2_Claims_Tests/RepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Challenge2_Claims_Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
